230c51ab6c01d3987e57f1c1d404bb42623139e3
Neil <neilmchenry@gmail.com>
2019-07-21 21:12:42 -0500

    Reorganized Files, .gitignore
    Exclude local files, intermediate files, and binaries that can b e generated locally by the Unreal Engine

---
+++ Plugins/VRExpansionPlugin/VRExpansionPlugin/Intermediate/Build/Win64/UE4Editor/Inc/VRExpansionPlugin/GS_LerpToHand.generated.h
@@ -0,0 +1,99 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef VREXPANSIONPLUGIN_GS_LerpToHand_generated_h
+#error "GS_LerpToHand.generated.h already included, missing '#pragma once' in GS_LerpToHand.h"
+#endif
+#define VREXPANSIONPLUGIN_GS_LerpToHand_generated_h
+
+#define VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_10_DELEGATE \
+static inline void FVRLerpToHandFinishedSignature_DelegateWrapper(const FMulticastScriptDelegate& VRLerpToHandFinishedSignature) \
+{ \
+	VRLerpToHandFinishedSignature.ProcessMulticastDelegate<UObject>(NULL); \
+}
+
+
+#define VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_RPC_WRAPPERS
+#define VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_RPC_WRAPPERS_NO_PURE_DECLS
+#define VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesUGS_LerpToHand(); \
+	friend struct Z_Construct_UClass_UGS_LerpToHand_Statics; \
+public: \
+	DECLARE_CLASS(UGS_LerpToHand, UVRGripScriptBase, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UGS_LerpToHand)
+
+
+#define VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_INCLASS \
+private: \
+	static void StaticRegisterNativesUGS_LerpToHand(); \
+	friend struct Z_Construct_UClass_UGS_LerpToHand_Statics; \
+public: \
+	DECLARE_CLASS(UGS_LerpToHand, UVRGripScriptBase, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/VRExpansionPlugin"), NO_API) \
+	DECLARE_SERIALIZER(UGS_LerpToHand)
+
+
+#define VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API UGS_LerpToHand(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UGS_LerpToHand) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UGS_LerpToHand); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UGS_LerpToHand); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UGS_LerpToHand(UGS_LerpToHand&&); \
+	NO_API UGS_LerpToHand(const UGS_LerpToHand&); \
+public:
+
+
+#define VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API UGS_LerpToHand(UGS_LerpToHand&&); \
+	NO_API UGS_LerpToHand(const UGS_LerpToHand&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, UGS_LerpToHand); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(UGS_LerpToHand); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(UGS_LerpToHand)
+
+
+#define VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_PRIVATE_PROPERTY_OFFSET
+#define VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_29_PROLOG
+#define VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_PRIVATE_PROPERTY_OFFSET \
+	VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_RPC_WRAPPERS \
+	VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_INCLASS \
+	VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_PRIVATE_PROPERTY_OFFSET \
+	VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_RPC_WRAPPERS_NO_PURE_DECLS \
+	VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_INCLASS_NO_PURE_DECLS \
+	VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h_32_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID VEEP_Plugins_VRExpansionPlugin_VRExpansionPlugin_Source_VRExpansionPlugin_Public_GripScripts_GS_LerpToHand_h
+
+
+#define FOREACH_ENUM_EVRLERPINTERPOLATIONMODE(op) \
+	op(EVRLerpInterpolationMode::QuatInterp) \
+	op(EVRLerpInterpolationMode::EulerInterp) \
+	op(EVRLerpInterpolationMode::DualQuatInterp) 
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- River_Splines_Backup/Intermediate/Build/Win64/River_SplinesEditor/Development/Engine/SharedPCH.Engine.cpp
+++
@@ -1,1 +0,0 @@
-#include "C:/Users/Kyle/Documents/Unreal Projects/River_Splines_Backup/Intermediate/Build/Win64/River_SplinesEditor/Development/Engine/SharedPCH.Engine.h"
